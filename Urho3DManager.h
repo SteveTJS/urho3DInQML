b45ae4beae2971386e9175c19c2271006327a99e
SteveTJS <compte_sites_programmation@hotmail.fr>
2017-10-20 16:54:21 +0200

    Update Urho3DManager.h

--- Urho3DManager.h
+++ Urho3DManager.h
@@ -17,16 +17,18 @@ class Urho3DManager : public QObject
 
 private:
     QTimer *mTimer;
-    unsigned char* mRenderTextureData;
     int mUrho3DWidth;
     int mUrho3DHeight;
 
     //to provide QML part the Urho3D texture
     TextureProvider *mTextureProvider;
 
+    unsigned char* mRenderTextureData;
+    
     //Urho3D application (exec engine, create Urho3D window, scene, ...)
     Urho3D::SharedPtr<Urho3DApplication> mUrho3DApplication;
     Urho3D::SharedPtr<Urho3D::Context> mUrho3DContext;
+    bool mIsUrho3DCreated;
 
 public:
     explicit Urho3DManager(QQmlApplicationEngine *inEngine=0, QObject *parent = 0);
